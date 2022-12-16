61c73b9e1d0bdf662ba404a24ad8f0f60efa2fdf
JohnGuerreroCor <john.guerrero@usco.edu.co>
2022-12-16 11:17:38 -0500

    Cambios al css admin

--- src/app/components/pages/estudiantes/articulosestudiantes/articuloe/articuloe.component.ts
+++ src/app/components/pages/estudiantes/articulosestudiantes/articuloe/articuloe.component.ts
@@ -1,228 +1,228 @@
-import { Component, OnInit } from '@angular/core';
-import { DatePipe } from '@angular/common';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FormGroup, NgForm } from '@angular/forms';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { PlantelService } from '../../../../../services/profesores/plantel/plantel.service';
-import { Plantel } from '../../../../../interfaces/profesores/plantel/plantel';
-import { AngularFireAuth } from '@angular/fire/auth';
-import 'firebase/auth';
-import { ToastrService } from 'ngx-toastr';
-import { Articulo } from '../../../../../interfaces/profesores/articulo';
-import { map } from 'rxjs/operators';
-import { ArticulosestuService } from '../../../../../services/estudiantes/articulos/articulosestu.service';
+import { Component, OnInit } from "@angular/core";
+import { DatePipe } from "@angular/common";
+import { Router, ActivatedRoute } from "@angular/router";
+import { FormGroup, NgForm } from "@angular/forms";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { PlantelService } from "../../../../../services/profesores/plantel/plantel.service";
+import { Plantel } from "../../../../../interfaces/profesores/plantel/plantel";
+import { AngularFireAuth } from "@angular/fire/auth";
+import "firebase/auth";
+import { ToastrService } from "ngx-toastr";
+import { Articulo } from "../../../../../interfaces/profesores/articulo";
+import { map } from "rxjs/operators";
+import { ArticulosestuService } from "../../../../../services/estudiantes/articulos/articulosestu.service";
 
 @Component({
-  selector: 'app-articuloe',
-  templateUrl: './articuloe.component.html',
-  styleUrls: ['./articuloe.component.css'],
+  selector: "app-articuloe",
+  templateUrl: "./articuloe.component.html",
+  styleUrls: ["./articuloe.component.css"],
   providers: [DatePipe],
 })
 export class ArticuloeComponent implements OnInit {
   pdfSource =
-  'https://drive.google.com/file/d/1unOARuer2zVmdsWISIXgDH-kDgVtgOdg/preview';
-page = 1;
-pageSize = 3;
-pageArticulos = 1;
-pageSizeArticulos = 3;
-pageArticulosProfesores = 1;
-pageSizeArticulosProfesores = 3;
-today = new Date();
-fecha: any;
-closeResult: string;
-defaultImgUrl: any;
-urlimg: any;
-war: any;
-modalReference: any;
-error = false;
-passError = '';
-imgError = '';
-alertBool = false;
-forma: FormGroup;
-controls: any;
-nuevo = false;
-id: string;
-idx: string;
-newAttribute: any = {};
-articulos: any = [];
-articuloEstudiante: Articulo = {
-  anio: '',
-  autores: '',
-  enlace: '',
-  nombreArticulo: '',
-  resumen: '',
-  revista: '',
-};
-plantelProfesor: Plantel = {
-  foto: '',
-  nombre: '',
-  sintesis: '',
-  fieldArray: [],
-  fieldArrayArticulos: [],
-  correo: '',
-  fecha: '',
-  cvlac: '',
-  orcid: '',
-};
-slides: any = [[]];
+    "https://drive.google.com/file/d/1unOARuer2zVmdsWISIXgDH-kDgVtgOdg/preview";
+  page = 1;
+  pageSize = 3;
+  pageArticulos = 1;
+  pageSizeArticulos = 3;
+  pageArticulosProfesores = 1;
+  pageSizeArticulosProfesores = 3;
+  today = new Date();
+  fecha: any;
+  closeResult: string;
+  defaultImgUrl: any;
+  urlimg: any;
+  war: any;
+  modalReference: any;
+  error = false;
+  passError = "";
+  imgError = "";
+  alertBool = false;
+  forma: FormGroup;
+  controls: any;
+  nuevo = false;
+  id: string;
+  idx: string;
+  newAttribute: any = {};
+  articulos: any = [];
+  articuloEstudiante: Articulo = {
+    anio: "",
+    autores: "",
+    enlace: "",
+    nombreArticulo: "",
+    resumen: "",
+    revista: "",
+  };
+  plantelProfesor: Plantel = {
+    foto: "",
+    nombre: "",
+    sintesis: "",
+    fieldArray: [],
+    fieldArrayArticulos: [],
+    correo: "",
+    fecha: "",
+    cvlac: "",
+    orcid: "",
+  };
+  slides: any = [[]];
 
-vistaEdicion = false;
-acumFechas = 0;
-comodinAcum = 0;
-loading = true;
-link: any;
+  vistaEdicion = false;
+  acumFechas = 0;
+  comodinAcum = 0;
+  loading = true;
+  link: any;
 
-constructor(
-  private toastr: ToastrService,
-  public auth: AngularFireAuth,
-  public datepipe: DatePipe,
-  private modalService: NgbModal,
-  private plantelService: PlantelService,
-  private router: Router,
-  private activatedRoute: ActivatedRoute,
-  private articulosEstuService: ArticulosestuService
-) {
-  this.activatedRoute.params.subscribe((parametros) => {
-    console.log(parametros);
-    this.id = parametros.id;
-    this.idx = parametros.idx;
-    this.link = parametros.id;
-    if (this.id !== 'nuevo') {
-    }
-    this.articulosEstuService
-      .getArticuloEstudiante(this.id)
-      .subscribe(
-        (articuloEstudiante) => (this.articuloEstudiante = articuloEstudiante)
-      );
-  });
-}
-get sortData() {
-  return this.articulos.sort((a, b) => {
-    // tslint:disable-next-line:whitespace
-    // tslint:disable-next-line:no-angle-bracket-type-assertion
-    return <any> new Date(b.anio) - <any> new Date(a.anio);
-  });
-}
-obtenerArticulos(): void {
-  this.articulosEstuService
-    .getAll()
-    .snapshotChanges()
-    .pipe(
-      map((changes) =>
-        changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))
-      )
-    )
-    .subscribe((data) => {
-      this.articulos = data;
-    });
-}
-nav() {
-  this.router.navigate(['/admi_plantel', this.link]);
-}
-ngOnInit() {
-  this.fecha = this.datepipe.transform(this.today, 'dd/MM/yyyy');
-  this.obtenerArticulos();
-}
-showSuccess() {
-  this.toastr.success('Acción exitosa', 'Elemento guardado', {
-    timeOut: 2500,
-  });
-}
-showDanger() {
-  this.toastr.error('Intenten nuevamente', 'Error al guardar', {
-    timeOut: 2500,
-  });
-}
-showInfo() {
-  this.toastr.info('', 'Elemento actualizado', {
-    timeOut: 2500,
-  });
-}
-showWarning() {
-  this.toastr.warning('Intenten nuevamente', 'Error al actualizar', {
-    timeOut: 2500,
-  });
-}
-elementoAgregado() {
-  this.toastr.info('', 'Elemento agregado', {
-    timeOut: 2500,
-  });
-}
-elementoEliminado() {
-  this.toastr.warning('', 'Elemento eliminado', {
-    timeOut: 2500,
-  });
-}
-openModal(confirmar) {
-  this.modalReference = this.modalService.open(confirmar, {
-    centered: true,
-    size: 'sm',
-    backdrop: 'static',
-    windowClass: 'fade-in',
-  });
-}
-openSm(formAdmin) {
-  this.modalReference = this.modalService.open(formAdmin, {
-    size: 'sm',
-    centered: true,
-    backdrop: 'static',
-  });
-}
-verProfesor(idx: number) {
-  this.router.navigate(['/docente', idx]);
-}
-up() {
-  window.scroll(0, 400);
-}
-guardar() {
-  if (
-    this.plantelProfesor.nombre !== this.war ||
-    this.plantelProfesor.nombre !== this.war
+  constructor(
+    private toastr: ToastrService,
+    public auth: AngularFireAuth,
+    public datepipe: DatePipe,
+    private modalService: NgbModal,
+    private plantelService: PlantelService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private articulosEstuService: ArticulosestuService
   ) {
-    this.error = false;
-    console.log(this.plantelProfesor.nombre);
-    console.log(this.war);
-    this.modalReference.close();
-    if (this.id === 'nuevo') {
-      this.plantelService.nuevoPlantel(this.plantelProfesor).subscribe(
-        (data) => {
-          this.router.navigate(['/docentes']);
-          this.modalReference.close();
-        },
-        (error) => console.error(error)
-      );
-    } else {
-      this.modalReference.close();
-      this.plantelService
-        .actualizarPlantel(this.plantelProfesor, this.id)
+    this.activatedRoute.params.subscribe((parametros) => {
+      console.log(parametros);
+      this.id = parametros.id;
+      this.idx = parametros.idx;
+      this.link = parametros.id;
+      if (this.id !== "nuevo") {
+      }
+      this.articulosEstuService
+        .getArticuloEstudiante(this.id)
         .subscribe(
+          (articuloEstudiante) => (this.articuloEstudiante = articuloEstudiante)
+        );
+    });
+  }
+  get sortData() {
+    return this.articulos.sort((a, b) => {
+      // tslint:disable-next-line:whitespace
+      // tslint:disable-next-line:no-angle-bracket-type-assertion
+      return <any>new Date(b.anio) - <any>new Date(a.anio);
+    });
+  }
+  obtenerArticulos(): void {
+    this.articulosEstuService
+      .getAll()
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))
+        )
+      )
+      .subscribe((data) => {
+        this.articulos = data;
+      });
+  }
+  nav() {
+    this.router.navigate(["/admi_articulosEstudiantes", this.link]);
+  }
+  ngOnInit() {
+    this.fecha = this.datepipe.transform(this.today, "dd/MM/yyyy");
+    this.obtenerArticulos();
+  }
+  showSuccess() {
+    this.toastr.success("Acción exitosa", "Elemento guardado", {
+      timeOut: 2500,
+    });
+  }
+  showDanger() {
+    this.toastr.error("Intenten nuevamente", "Error al guardar", {
+      timeOut: 2500,
+    });
+  }
+  showInfo() {
+    this.toastr.info("", "Elemento actualizado", {
+      timeOut: 2500,
+    });
+  }
+  showWarning() {
+    this.toastr.warning("Intenten nuevamente", "Error al actualizar", {
+      timeOut: 2500,
+    });
+  }
+  elementoAgregado() {
+    this.toastr.info("", "Elemento agregado", {
+      timeOut: 2500,
+    });
+  }
+  elementoEliminado() {
+    this.toastr.warning("", "Elemento eliminado", {
+      timeOut: 2500,
+    });
+  }
+  openModal(confirmar) {
+    this.modalReference = this.modalService.open(confirmar, {
+      centered: true,
+      size: "sm",
+      backdrop: "static",
+      windowClass: "fade-in",
+    });
+  }
+  openSm(formAdmin) {
+    this.modalReference = this.modalService.open(formAdmin, {
+      size: "sm",
+      centered: true,
+      backdrop: "static",
+    });
+  }
+  verProfesor(idx: number) {
+    this.router.navigate(["/docente", idx]);
+  }
+  up() {
+    window.scroll(0, 400);
+  }
+  guardar() {
+    if (
+      this.plantelProfesor.nombre !== this.war ||
+      this.plantelProfesor.nombre !== this.war
+    ) {
+      this.error = false;
+      console.log(this.plantelProfesor.nombre);
+      console.log(this.war);
+      this.modalReference.close();
+      if (this.id === "nuevo") {
+        this.plantelService.nuevoPlantel(this.plantelProfesor).subscribe(
           (data) => {
-            this.router.navigate(['/docentes']);
+            this.router.navigate(["/docentes"]);
             this.modalReference.close();
           },
           (error) => console.error(error)
         );
-    }
-  } else {
-    this.error = true;
-    this.passError = 'Formulario incompleto.';
-    this.modalReference.close();
-  }
-}
-agregarNuevo(forma: NgForm) {
-  this.router.navigate(['/admi_plantel', 'nuevo']);
-  forma.reset({});
-}
-borrarPlantel() {
-  this.plantelService.borrarPlantel(this.id).subscribe((respuesta) => {
-    if (respuesta) {
-      console.error(respuesta);
+      } else {
+        this.modalReference.close();
+        this.plantelService
+          .actualizarPlantel(this.plantelProfesor, this.id)
+          .subscribe(
+            (data) => {
+              this.router.navigate(["/docentes"]);
+              this.modalReference.close();
+            },
+            (error) => console.error(error)
+          );
+      }
     } else {
-      this.router.navigate(['/docentes']);
-      // delete this.plantelProfesores[this.id];
-      this.elementoEliminado();
+      this.error = true;
+      this.passError = "Formulario incompleto.";
       this.modalReference.close();
     }
-  });
-}
+  }
+  agregarNuevo(forma: NgForm) {
+    this.router.navigate(["/admi_plantel", "nuevo"]);
+    forma.reset({});
+  }
+  borrarPlantel() {
+    this.plantelService.borrarPlantel(this.id).subscribe((respuesta) => {
+      if (respuesta) {
+        console.error(respuesta);
+      } else {
+        this.router.navigate(["/docentes"]);
+        // delete this.plantelProfesores[this.id];
+        this.elementoEliminado();
+        this.modalReference.close();
+      }
+    });
+  }
 }
